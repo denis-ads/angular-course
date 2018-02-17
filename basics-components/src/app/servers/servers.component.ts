1505518d1c2bf34fd094658ba12eccecddcaa871
DENIS APOLINARIO DA SILVA <denis.apolinario@viavarejo.com.br>
2018-02-17 14:39:58 -0200

    first commit

---
+++ basics-components/src/app/servers/servers.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  // selector: '.app-servers',
+  selector: 'app-servers',
+  // template: `
+  //   <app-server></app-server>
+  //   <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allownNewServer:boolean =false;
+  serverCreationStatus = 'No server was created';
+
+  constructor() {
+
+    setTimeout(()=>{
+      this.allownNewServer = true
+    },2000)
+  }
+
+  ngOnInit() {
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'Server was created';
+  }
+
+}
